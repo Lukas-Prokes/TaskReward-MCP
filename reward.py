4e450bb9e6a34864c566c12c841b46394d637c62
Lukas-Prokes <lukasprokes.contact@gmail.com>
2026-04-18 12:27:51 +0200

    feat: add reward script with overflow protection

---
+++ reward.py
@@ -0,0 +1,11 @@
+print("Congratulations! You've completed a task and earned 20 energy points!")
+with open('data/energy.json', 'r') as f:
+        data = json.load(f)
+
+data['current_energy'] += 20
+if data['current_energy'] > data['max_energy']:
+    data['current_energy'] = data['max_energy']
+
+with open('data/energy.json', 'w') as f:
+    json.dump(data, f, indent=4)
+print(f'Your current energy is now: {data["current_energy"]}')
